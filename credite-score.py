b741b991a1d0794d49c40ed728ba501f532c8fea
khikisb <89850965+khikisb@users.noreply.github.com>
2022-11-16 15:56:13 +0700

    Update credite-score.py

--- credite-score.py
+++ credite-score.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import scikit-learn
 
 from scikit-learn.preprocessing import MinMaxScaler
 from scikit-learn.model_selection import train_test_split
